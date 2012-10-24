b6ee3f17c5ea93c8ef80fd0c9b2139a73b18caac
iknowicouldalwaysturn <iknowicouldalwaysturn@outlook.com>
2012-10-24 14:23:10 -0400

    Deleting All Rails files that were directly coded
    Rails files that weren't directly edited are deleted

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Afriendlysyncingapp::Application.config.secret_token = '533eebe59678cffc39b78f9e48936e6431bcdb769d5b7b5e5a4c9108776b45354a66c8fd3521a1c87e7bb4b764ee5385b6aa38e5bba38847568aa51d15c38d74'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Afriendlysyncingapp::Application.config.session_store :cookie_store, key: '_afriendlysyncingapp_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Afriendlysyncingapp::Application.config.session_store :active_record_store
