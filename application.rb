389c81c88f4ed6de608d68db92e177b4aea7c07e
iknowicouldalwaysturn <iknowicouldalwaysturn@outlook.com>
2012-10-24 14:38:33 -0400

    Code updated to actually work with Sinatra plus optimization

--- application.rb
+++ application.rb
@@ -20,41 +20,57 @@ helpers do
   end
 end
 
-post '/api/fb/pull/:thread' do  
-  thread_id = params[ :thread ]
-  @from = params[ :from ]
+post '/api/fb/pull/:thread' do
+  thread_nickname = params[ :thread ]
+  
+  # Parameter from post request
+  instant_message_body = params[ :body ]
+  
+  # This is how the Partychat hook sends data. Not good for us so have to parse it
+  instant_message_extraction = instant_message_body.match( /^\[(?<from>\S+)\] \/[\S]+ (?<body>.+)/ )
+  sender_object = FbMember.find_by_google_talk_name( instant_message_extraction[ :from ] )
+  send_to_facebook_content = "*#{sender_object.name}:* #{instant_message_extraction[ :body ]}"
+  
+  # Posting to Facebook
     
+  require 'mechanize'
   @agent = Mechanize.new
     
-  cookie_location = "#{Rails.root}/tmp/fb_cookie.yml"
+  cookie_location = './tmp/fb_cookie.yml'
   if File.exist? cookie_location
-    @agent.cookie_jar.load( "#{Rails.root}/tmp/fb_cookie.yml" )
+    @agent.cookie_jar.load( './tmp/fb_cookie.yml' )
   end
-    
+  
   facebook_site = @agent.get( 'http://m.facebook.com/messages' )
-    
+  
+  # Check to see if url has redirected to login page
+  # If redirected, then it means not logged in  
   current_url = facebook_site.uri.to_s
   current_path = URI.parse( current_url ).path
-    
+  
+  # Log in and get fresh cookie if having to login again
   if !current_path.include? 'messages'
+    #@TODO check to see if actually logged in, otherwise log/report the problem and stop code
     facebook_site = login_and_save_cookie( facebook_site )
   end
-    
-  thread_id = FbThread.find_by_nickname( thread_id )
-  individual_thread_url = "https://m.facebook.com/messages/read?action=read&tid=id.#{thread_id}"
+  
+  # Find the thread name parameter from url in database and go to the thread
+  thread_object = FbThread.find_with_receive_endpoint( thread_nickname )
+  if thread_object.nil?
+    return
+  end
+  thread_id = thread_object.fb_id
+  individual_thread_url = "https://m.facebook.com/messages/read?action=read&tid=id.#{thread_id}"  
   individual_thread = @agent.get( individual_thread_url )
-    
-  instant_message_body = params[ :body ]
-    
-  instant_message_extraction = instant_message_body.match( /^\[(?<from>\S+)\] \/[\S]+ (?<body>.+)/ )    
-  send_to_facebook_content = "*#{instant_message_extraction[ :from ]}:* #{instant_message_extraction[ :body ]}"
-    
+  
+  # Post to the message text area on specific thread page
   reply_form = individual_thread.form_with( :id => 'composer_form' )
-  reply_form[ 'body' ] = content
+  reply_form[ 'body' ] = send_to_facebook_content
   reply_form.submit
 end
 
 get '/api/fb/push/threads' do
+  require 'koala'
   me = Koala::Facebook::API.new( ENV[ 'fb_api' ] )
     
   # Get all the threads and latest messages at once for account to save time from polling FB API for each thread
@@ -102,9 +118,12 @@ get '/api/fb/push/threads' do
     end
       
     # Update the database with the last message id that was pushed for the thread
-    @fb_thread_from_database.update_column( 'last_message_id', @message_id )
-      
+    if @message_id != @fb_thread_from_database.last_message_id      
+      @fb_thread_from_database.update_column( 'last_message_id', @message_id )
+    end
+    "Done with #{@fb_thread_from_database.nickname}"
   end
+  "Done"
 end
 
 # Eh
