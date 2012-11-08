909f7dcdcdc996a174f5850e1d5ba29e005501b6
iknowicouldalwaysturn <iknowicouldalwaysturn@outlook.com>
2012-11-08 17:16:21 -0500

    Slight cleaning up and commenting of code

--- application.rb
+++ application.rb
@@ -11,11 +11,9 @@ helpers do
     
     login_form = page.form_with( :id => 'login_form' )
     
-    #@TODO dont have email/password publicly here
     login_form[ 'email' ] = ENV[ 'fb_user' ]
     login_form[ 'pass' ] = ENV[ 'fb_pass' ]
     page = login_form.submit# 'login'
-    #@TODO test to make sure page logged in
     if File.exist? cookie_location
       File.delete cookie_location
     end
@@ -47,14 +45,11 @@ post '/api/fb/pull/:thread' do
   
   facebook_site = @agent.get( 'http://m.facebook.com/messages' )
   
-  # Check to see if url has redirected to login page
-  # If redirected, then it means not logged in  
+  # Check to see if url has redirected to login page. If so, then it means not logged in.
   current_url = facebook_site.uri.to_s
-  current_path = URI.parse( current_url ).path
-  
-  # Log in and get fresh cookie if having to login again
+  current_path = URI.parse( current_url ).path  
+  # If cookie doesn't have you signed in, manually log in and get fresh cookie
   if !current_path.include? 'messages'
-    #@TODO check to see if actually logged in, otherwise log/report the problem and stop code
     facebook_site = login_and_save_cookie( facebook_site )
   end
   
@@ -66,7 +61,8 @@ post '/api/fb/pull/:thread' do
   
   thread_id = thread_object.fb_id
   
-  # @TODO This is shameful, lol. One of the threads has different api and actual url ids. But current database table only supports one id number. So this is a quick hack for fixing the one thread that has a problem. This definitely needs to be changed by having a second column for the id and be for all columns instead of a hack for one
+  # One thread has different api and actual url id. Current db supports one id. Currently hard coded hack.
+  # Should have two columns in table, one for api and another for url id
   if thread_object.id == 5
     thread_id = ENV[ 'fb_thread_id' ]
   end
